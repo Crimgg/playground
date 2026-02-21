33c2dbe85774a4800bce8a4e78a62fe0db5ade50
Crimgg <lucasthecroissant@gmail.com>
2026-02-21 21:52:20 +0000

    end

--- play.py
+++ play.py
@@ -1,20 +1,39 @@
 import random
-value = random.randint(1, 100)
+max = random.randint(10000, 100000)
+factorange = max/200
+
+while True:   
+    value = random.randint(1, max)
+    factors = []
+    for i in range(2, value):
+        if value%i == 0:
+            factors.append(i)
+    if len(factors) >= 5:
+        break
+
 guess = 0 
-print("Guess a number between 1-100: ")
+print(f"Guess a number between 1-{max}: ")
 while guess != value:
     try:
-        guess = int(input(""))
+        inp = input("")
+        if inp == "I give up":
+            print(f"The answer was {value}")
+            break
+        guess = int(inp)
     except:
         print("Type a number")
         continue 
     if guess == value:
-        print("correct")
+        print("Correct")
+        break
     if guess > value:
         print("Too high")
     if guess < value:
         print("Too low")
-    if guess >= 101:
+    if guess > max:
         print("Outside of Boundry")
     if guess <= 0:
         print("Outside of Boundry")
+    if abs(guess - value) <= factorange:
+        f = factors[random.randint(0, len(factors) - 1)]
+        print (f"{f} is a factor")
